b0126aaf987dfeaa38b037d03f97c12b43169408
Paul Sczurek <pocketcalculatorshow@gmail.com>
2018-01-30 16:50:39 -0500

    added bodyparser and router const definitions

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const bodyParser = require('body-parser');
+const router = express.Router();
 const app = express();
 
 const blogPostRouter = require('./blogpost');
