95d0dfae51901fc26a123dd35eaaf67ed79b4d17
Paul Sczurek <pocketcalculatorshow@gmail.com>
2018-01-30 16:07:03 -0500

    GET posts by ID

--- blogpost/router.js
+++ blogpost/router.js
@@ -11,6 +11,26 @@ const app = express();
 app.use(morgan('common'));
 app.use(bodyParser.json());
 
-router.get('/', (req, res) => {
-  res.json(Blogposts.get());
+app.get('/posts', (req, res) => {
+  BlogPost
+    .find(filters)
+    .then(BlogPosts => res.json(
+            BlogPosts.map(blogpost => blogpost.serialize())
+    ))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
+    });
+});
+// can also request by ID
+app.get('/posts/:id', (req, res) => {
+  BlogPost
+    // this is a convenience method Mongoose provides for searching
+    // by the object _id property
+    .findById(req.params.id)
+    .then(blogpost => res.json(blogpost.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
+    });
 });
