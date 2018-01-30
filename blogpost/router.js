a3f7f6a70e109b4a633e6b62350a27e8ca96d489
Paul Sczurek <pocketcalculatorshow@gmail.com>
2018-01-30 15:43:40 -0500

    Added DB connection details, loading morgan and bodyparser

---
+++ blogpost/router.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const { PORT, DATABASE_URL } = require('./config');
+const { BlogPost } = require('./model');
+
+const app = express();
+// load morgan for logging
+app.use(morgan('common'));
+app.use(bodyParser.json());
